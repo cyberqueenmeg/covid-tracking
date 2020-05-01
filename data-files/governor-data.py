2cde2632ac7a8d1bf291c36fa2f622139082add4
adenarussel <64279464+adenarussel@users.noreply.github.com>
2020-05-01 13:20:40 -0400

    Update governor-data.py

--- data-files/governor-data.py
+++ data-files/governor-data.py
@@ -1,3 +1,4 @@
 print("Governor Data")
 
 #CA governor (Gavin Newsom) twitter feed: https://twitter.com/GavinNewsom?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
+#CT governor (Ned Lamont) twitter feed: https://twitter.com/GovNedLamont?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
