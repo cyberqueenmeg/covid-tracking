7a7687e570c08acbed59b202ae3ee27b08accc3d
adenarussel <64279464+adenarussel@users.noreply.github.com>
2020-05-01 13:26:00 -0400

    Update case-data.py

--- data-files/case-data.py
+++ data-files/case-data.py
@@ -1,2 +1,3 @@
 print("Case Data")
 print("Arizona case data pullled from: https://www.azfamily.com/news/interactives/search-coronavirus-cases-in-arizona-by-zip-code/article_d9291aca-7cf0-11ea-85e7-c71a704affdc.html")
+print("Connectict case data pulled from: https://www.nbcconnecticut.com/news/coronavirus/coronavirus-in-connecticut-interactive-latest-town-by-town-case-numbers/2263113/)
