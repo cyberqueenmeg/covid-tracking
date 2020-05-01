44a5120932938ffe9b3b16387c5668f517056999
AnjaliPrabhu848 <63516271+AnjaliPrabhu848@users.noreply.github.com>
2020-05-01 10:59:18 -0700

    Update mayor-data.py

--- data-files/mayor-data.py
+++ data-files/mayor-data.py
@@ -1,4 +1,4 @@
 print ("Mayor Data")
 
-# CA mayor (Lily Mei) twitter feed: https://twitter.com/lilymei4fremont?lang=en
+# Fremont mayor (Lily Mei) twitter feed: https://twitter.com/lilymei4fremont?lang=en
 # City of Stamford twitter feed: https://twitter.com/CityofStamford?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
