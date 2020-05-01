dfbfc28f1c6b0485af1cc250d342b161b9dfe353
ocov <64279485+ocov@users.noreply.github.com>
2020-04-30 21:04:46 -0700

    Update case-data.py

--- data-files/case-data.py
+++ data-files/case-data.py
@@ -1,1 +1,2 @@
 print("Case Data")
+print("Arizona case data pullled from: https://www.azfamily.com/news/interactives/search-coronavirus-cases-in-arizona-by-zip-code/article_d9291aca-7cf0-11ea-85e7-c71a704affdc.html")
