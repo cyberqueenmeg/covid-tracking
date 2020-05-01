2a8964dc6811972ecf78bde32cfbc828c558556c
adenarussel <64279464+adenarussel@users.noreply.github.com>
2020-05-01 13:24:12 -0400

    Update restaurant-data.py

--- data-files/restaurant-data.py
+++ data-files/restaurant-data.py
@@ -1,2 +1,3 @@
 print("Restaurant Data")
 print("Arizona restaurant data retrieved from: https://www.yelp.com/search?find_desc=&find_loc=Arizona")
+print("Connecticut restarant data retrieved from: https://www.yelp.com/search?find_desc=Restaurants&find_loc=Connecticut%2C%20CT&ed_attrs=PlatformPickup%2CPlatformDelivery)
