0f984df9e76d89212b4658b1a7eb8f2c38b4f393
ocov <64279485+ocov@users.noreply.github.com>
2020-06-10 11:52:07 -0700

    Update mayor-data.py

--- data-files/mayor-data.py
+++ data-files/mayor-data.py
@@ -2,3 +2,5 @@ print ("Mayor Data")
 
 # Fremont mayor (Lily Mei) twitter feed: https://twitter.com/lilymei4fremont?lang=en
 # City of Stamford twitter feed: https://twitter.com/CityofStamford?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
+# Salt Lake City mayor (Erin Mendenhall) twitter feed:
+# Anchorage mayor (Ethan Berkowitz) twitter feed:
