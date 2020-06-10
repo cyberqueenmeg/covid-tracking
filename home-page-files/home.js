d3c8e5c3ee59aaf8fbd8b3978601806dce1f1b8e
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2020-06-10 10:16:21 -0700

    FIXED!!! (I just need to do one more thing)

--- home-page-files/home.js
+++ home-page-files/home.js
@@ -1,14 +1,16 @@
 function changeWebPage() {
-	var zipcode = document.getElementById("zipcode").value;
-	if (zipcode = "85028") {
-		window.location.assign("../zip-code-files/85028.html");
-	} else if (zipcode = "06903") {
+	console.log("function is running");
+	var zipcode = document.getElementById("zip-code").value;
+	
+	if (zipcode === "06903") {
 		window.location.assign("../zip-code-files/06903.html");
-	} else if (zipcode = "85032") {
+	} else if (zipcode === "85028") {
+		window.location.assign("../zip-code-files/85028.html");
+	} else if (zipcode === "85032") {
 		window.location.assign("../zip-code-files/85032.html");
-	} else if (zipcode = "85254") {
+	} else if (zipcode === "85254") {
 		window.location.assign("../zip-code-files/85254.html");
-	} else if (zipcode = "94538") {
+	} else if (zipcode === "94538") {
 		window.location.assign("../zip-code-files/94538.html");
 	} else {
 		print("Error");
