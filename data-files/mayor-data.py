85657c4663b9409ea68fcccb1cf1a43b2ec99d7f
ocov <64279485+ocov@users.noreply.github.com>
2020-06-12 11:16:46 -0700

    Update mayor-data.py

--- data-files/mayor-data.py
+++ data-files/mayor-data.py
@@ -4,3 +4,5 @@ print ("Mayor Data")
 # City of Stamford twitter feed: https://twitter.com/CityofStamford?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
 # Salt Lake City mayor (Erin Mendenhall) twitter feed:
 # Anchorage mayor (Ethan Berkowitz) twitter feed:
+# New York City mayor (Bill de Blasio) twitter feed:
+# Cedar City mayor (Wilson Edwards) twitter feed:
