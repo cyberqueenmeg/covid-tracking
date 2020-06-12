3af04e92e9138e59644735b08be5b2460a53d3a0
ocov <64279485+ocov@users.noreply.github.com>
2020-06-12 11:10:41 -0700

    Update governor-data.py

--- data-files/governor-data.py
+++ data-files/governor-data.py
@@ -4,3 +4,4 @@ print("Governor Data")
 #CT governor (Ned Lamont) twitter feed: https://twitter.com/GovNedLamont?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
 #AK governor (Mike Dunleavy) twitter feed: 
 #UT governor (Gary Herbert) twitter feed: 
+#NY governor (Andrew Cuomo) twitter feed: 
