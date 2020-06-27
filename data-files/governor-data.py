737b9bb200dc1d80de0c87beacefbf5928ade896
mhowell1 <53236323+mhowell1@users.noreply.github.com>
2020-06-26 17:51:25 -0700

    Delete governor-data.py

--- data-files/governor-data.py
+++
@@ -1,7 +0,0 @@
-print("Governor Data")
-
-#CA governor (Gavin Newsom) twitter feed: https://twitter.com/GavinNewsom?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
-#CT governor (Ned Lamont) twitter feed: https://twitter.com/GovNedLamont?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
-#AK governor (Mike Dunleavy) twitter feed: 
-#UT governor (Gary Herbert) twitter feed: 
-#NY governor (Andrew Cuomo) twitter feed: 
