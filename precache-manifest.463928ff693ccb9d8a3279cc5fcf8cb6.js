fc846a539c2cd1acd8d57c2f2f856c2254ea27eb
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-27 14:48:04 -0700

    Updates

---
+++ precache-manifest.463928ff693ccb9d8a3279cc5fcf8cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba73b98f0f96b5c98f0a512b9981e34c",
+    "url": "/covid-tracking/index.html"
+  },
+  {
+    "revision": "16a79f94e4fb01aa0e8e",
+    "url": "/covid-tracking/static/css/main.f2b7cb71.chunk.css"
+  },
+  {
+    "revision": "31cc045c0aa3058263c0",
+    "url": "/covid-tracking/static/js/2.1775b553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-tracking/static/js/2.1775b553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a79f94e4fb01aa0e8e",
+    "url": "/covid-tracking/static/js/main.b74034a5.chunk.js"
+  },
+  {
+    "revision": "3d7d26bc3e2c804d1ca7",
+    "url": "/covid-tracking/static/js/runtime-main.61b86f3d.js"
+  }
+]);
