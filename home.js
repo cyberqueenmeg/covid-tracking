0b165d98bbd28eb7541dbfb3a3c7d963a86cd18f
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2020-06-27 11:18:27 -0700

    Revert "Merge branch 'react-rebuild'"
    This reverts commit 58191fc0f99dc0b02fd455b407615ae99250627a, reversing
    changes made to 254677bf14901f4f6101c1dc21e897a1129a8d06.

---
+++ home.js
@@ -0,0 +1,71 @@
+function changeWebPage() {
+   var zipcode = document.getElementById("zip-code").value;
+
+   if (zipcode === "02155") {
+      window.location.assign("02155.html");
+   } else if (zipcode === "02909") {
+      window.location.assign("02909.html");
+   } else if (zipcode === "06903") {
+      window.location.assign("06903.html");
+   } else if (zipcode === "08701") {
+      window.location.assign("08701.html");
+   } else if (zipcode === "10458") {
+      window.location.assign("10458.html");
+   } else if (zipcode === "19120") {
+      window.location.assign("19120.html");
+   } else if (zipcode === "20175") {
+      window.location.assign("20175.html");
+   } else if (zipcode === "21215") {
+      window.location.assign("21215.html");
+   } else if (zipcode === "22434") {
+      window.location.assign("22434.html");
+   } else if (zipcode === "23464") {
+      window.location.assign("23464.html");
+   } else if (zipcode === "30044") {
+      window.location.assign("30044.html");
+   } else if (zipcode === "32301") {
+      window.location.assign("32301.html");
+   } else if (zipcode === "37013") {
+      window.location.assign("37013.html");
+   } else if (zipcode === "45321") {
+      window.location.assign("45321.html");
+   } else if (zipcode === "60625") {
+      window.location.assign("60625.html");
+   } else if (zipcode === "75034") {
+      window.location.assign("75034.html");
+   } else if (zipcode === "84111") {
+      window.location.assign("84111.html");
+   } else if (zipcode === "84721") {
+      window.location.assign("84721.html");
+   } else if (zipcode === "85027") {
+      window.location.assign("85027.html");
+   } else if (zipcode === "85028") {
+      window.location.assign("85028.html");
+   } else if (zipcode === "85032") {
+      window.location.assign("85032.html");
+   } else if (zipcode === "85254") {
+      window.location.assign("85254.html");
+   } else if (zipcode === "87111") {
+      window.location.assign("87111.html");
+   } else if (zipcode === "87124") {
+      window.location.assign("87124.html");
+   } else if (zipcode === "90011") {
+      window.location.assign("90011.html");
+   } else if (zipcode === "92335") {
+      window.location.assign("92335.html");
+   } else if (zipcode === "94112") {
+      window.location.assign("94112.html");
+   } else if (zipcode === "94538") {
+      window.location.assign("94538.html");
+   } else if (zipcode === "95823") {
+      window.location.assign("95823.html");
+   } else if (zipcode === "96818") {
+      window.location.assign("96818.html");
+   } else if (zipcode === "98115") {
+      window.location.assign("98115.html");
+   } else if (zipcode === "99516") {
+      window.location.assign("99516.html");
+   } else {
+      window.location.assign("error.html");
+   }
+}
