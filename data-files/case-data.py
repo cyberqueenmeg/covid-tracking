303b640eb2329bc3317edf2f4f56842ed0363433
mhowell1 <53236323+mhowell1@users.noreply.github.com>
2020-06-26 17:36:39 -0700

    Delete case-data.py

--- data-files/case-data.py
+++
@@ -1,7 +0,0 @@
-print("Case Data")
-print("Arizona case data pullled from: https://www.azfamily.com/news/interactives/search-coronavirus-cases-in-arizona-by-zip-code/article_d9291aca-7cf0-11ea-85e7-c71a704affdc.html")
-print("Connecticut case data pulled from: https://www.nbcconnecticut.com/news/coronavirus/coronavirus-in-connecticut-interactive-latest-town-by-town-case-numbers/2263113/)
-print("Utah case data pulled from: https://coronavirus.utah.gov/case-counts/")
-print("Utah-Salt Lake County data pulled from: https://slco.org/covid-19/map/")
-print("Alaska case data pulled from: https://coronavirus-response-alaska-dhss.hub.arcgis.com/")
-print("New York State case data pulled from: https://covid19tracker.health.ny.gov/views/NYS-COVID19-Tracker/NYSDOHCOVID-19Tracker-Map?%3Aembed=yes&%3Atoolbar=no&%3Atabs=n")
