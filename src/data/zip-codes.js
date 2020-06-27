2b92a5e6056d8673cf211c9cd3f68ccf26ee98e7
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-26 21:05:37 -0700

    chore: Create base data structure

---
+++ src/data/zip-codes.js
@@ -0,0 +1,292 @@
+const DEFAULT_LINK = "http://www.google.com";
+
+export default {
+   "02155": {
+      cases:
+         "https://www.nytimes.com/interactive/2020/us/massachusetts-coronavirus-cases.html",
+      links: [
+         {
+            title: "Charlie Baker's Twitter (Massachusetts's Governor)",
+            link:
+               "https://twitter.com/MassGovernor?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+         },
+         {
+            title: "Breanna Lungo-Koehn's Twitter (Medford's Mayor)",
+            link: "https://twitter.com/mayorlungokoehn?lang=en",
+         },
+      ],
+   },
+   "02909": {
+      cases:
+         "https://ri-department-of-health-covid-19-data-rihealth.hub.arcgis.com",
+      links: [
+         {
+            title: "Gina Raimondo's Twitter (Rhode Island's Governor)",
+            link:
+               "https://twitter.com/GovRaimondo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+         },
+         {
+            title: "Jorge Elorza's Twitter (Providence's Mayor)",
+            link:
+               "https://twitter.com/Jorge_Elorza?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+         },
+      ],
+   },
+   "06903": {
+      cases:
+         "https://www.nytimes.com/interactive/2020/us/connecticut-coronavirus-cases.html",
+      links: [
+         {
+            title: "Stamford Government's Twitter",
+            link:
+               "https://twitter.com/CityofStamford?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+         },
+         {
+            title: "Ned Lamont's Twitter (Connecticut's Governor)",
+            link:
+               "https://twitter.com/GovNedLamont?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+         },
+         {
+            title: "Stamford Government Restrictions and Advisory",
+            link: "https://www.stamfordct.gov/covid19",
+         },
+         {
+            title: "Stamford COVID-19 Newsletter",
+            link:
+               "https://www.stamfordct.gov/public-safety-health-welfare/pages/daily-situational-updates-for-covid-19",
+         },
+      ],
+   },
+   /*"08701": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "10458": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "19120": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "20175": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "21215": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "22434": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "23464": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "30044": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "32301": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "37013": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "45321": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "60625": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "75034": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "84111": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "84721": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "85027": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "85028": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "85032": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "85254": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "87111": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "87124": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "90011": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "92335": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "94112": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "94538": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "95823": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "96818": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "98115": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },
+   "99516": {
+      cases: "",
+      food: "",
+      links: [
+         { title: "", link: "" },
+         { title: "", link: "" },
+      ],
+   },*/
+};
